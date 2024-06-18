8e1d58556aa3e3b2e12d14abc5e071a92222b604
leoccom <leocom0218@gmail.com>
2024-06-17 19:16:41 -0700

    Initial Commit

--- book_report/settings.py
+++ book_report/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+hrv0qq07utipvzfbfy8y1z06&*260c_ahz(fxo3(^8^2eu21-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['book-report-dcdd1da453f0.herokuapp.com',]
 
 
 # Application definition
