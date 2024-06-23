166d15f71ecf55187441e223bc62f6c9f1a4ed3e
Hyunjong Shin <leocom0218@gmail.com>
2024-06-22 19:05:28 -0700

    Add .DS_Store to .gitignore; Update settings.py;

--- book_report/settings.py
+++ book_report/settings.py
@@ -86,11 +86,11 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    # "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
