6c956766e48f6a6d799f3435925f34aff2b5f915
Hyunjong Shin <leocom0218@gmail.com>
2024-06-24 13:10:46 -0700

    Update settings, add favicon assets, and modify templates, URLs, and forms

--- book_report/urls.py
+++ book_report/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path("reports/", include("reports.urls")),
     path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- comments/forms.py
+++ comments/forms.py
@@ -6,5 +6,5 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ["content"]
         widgets = {
-            "content": forms.Textarea(attrs={"placeholder": "Add a comment...", "rows": 2}),
+            "content": forms.Textarea(attrs={"placeholder": "Add a comment...", "rows": 1}),
         }
